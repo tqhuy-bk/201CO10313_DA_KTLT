6cd2a26e35bd4999e0480fb6e85b1b53296d071e
tqhuy-bk <57402879+tqhuy-bk@users.noreply.github.com>
2020-11-29 21:38:42 +0700

    Update crawl.py

--- lazada/crawl.py
+++ lazada/crawl.py
@@ -1,7 +1,7 @@
 from lxml import html
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup 
+
 import csv
 
 def get_html(url):
@@ -20,22 +20,22 @@ def get_html(url):
     html_source = browser.page_source
     browser.quit()
     return html_source
-
 if __name__ == '__main__':
-    url = ['https://www.lazada.vn/dien-thoai-di-dong/?spm=a2o4n.searchlistcategory.cate_1.1.6ba2bdd5Mxpfhj','https://www.lazada.vn/do-gia-dung-lon/?spm=a2o4n.home.cate_3.3.19056afeiKdxzX','https://www.lazada.vn/tv-video-am-thanh-thiet-bi-deo-cong-nghe/?spm=a2o4n.searchlistcategory.cate_3.1.27a2545cpgrRBx']
+    url = ['https://www.lazada.vn/do-gia-dung-lon/?page=4&spm=a2o4n.home.cate_3.3.2fa06afeMq2GR2']
     with open('output_test.scv','w', encoding="utf8") as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(['Title','Price Current','Price Original'])
         for x in url:
-
             html_tree = html.fromstring(get_html(x))
-
-    
             for product in html_tree.xpath("//div[@class='c2prKC']"):
                 title = product.xpath(".//div[@class='c16H9d']/a/text()")
                 price_current = product.xpath(".//span[@class='c13VH6']/text()")
                 price_original = product.xpath(".//span[@class='c1-B2V']/del[@class='c13VH6']/text()")
-                writer.writerow([title,price_current,price_original])
+                if len(price_original) == 0:
+                    writer.writerow([title[0],price_current[0],None])
+                else:
+                    writer.writerow([title[0],price_current[0],price_original[0]])
+                
     
 
     
