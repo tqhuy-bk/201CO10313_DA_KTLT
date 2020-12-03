45ebf1da7c921ebe15bf08a017c688630d7a2714
tqhuy-bk <57402879+tqhuy-bk@users.noreply.github.com>
2020-12-03 23:38:11 +0700

    format output use pandas

--- lazada/crawl.py
+++ lazada/crawl.py
@@ -1,8 +1,7 @@
 from lxml import html
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
-import csv
+import pandas
 
 def get_html(url):
     scroll_to_bottom = True
@@ -21,21 +20,26 @@ def get_html(url):
     browser.quit()
     return html_source
 if __name__ == '__main__':
-    url = ['https://www.lazada.vn/do-gia-dung-lon/?page=4&spm=a2o4n.home.cate_3.3.2fa06afeMq2GR2']
-    with open('output_test.scv','w', encoding="utf8") as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerow(['Title','Price Current','Price Original'])
-        for x in url:
-            html_tree = html.fromstring(get_html(x))
-            for product in html_tree.xpath("//div[@class='c2prKC']"):
-                title = product.xpath(".//div[@class='c16H9d']/a/text()")
-                price_current = product.xpath(".//span[@class='c13VH6']/text()")
-                price_original = product.xpath(".//span[@class='c1-B2V']/del[@class='c13VH6']/text()")
-                if len(price_original) == 0:
-                    writer.writerow([title[0],price_current[0],None])
-                else:
-                    writer.writerow([title[0],price_current[0],price_original[0]])
-                
+    url = ['https://www.lazada.vn/laptop/?page=3&spm=a2o4n.home.cate_1.3.4b576afeRhRFkd']
+    title = []
+    price = []
+    url_product = []
+    category = []
+    for x in url:
+        html_tree = html.fromstring(get_html(x))
+        category_url = html_tree.xpath("//div[@class='ant-col-20 ant-col-push-4 c1z9Ut']/div[@class='cUQuRr']/h1/text()")[0]
+        for product in html_tree.xpath("//div[@class='c2prKC']"):
+            title_temp = product.xpath(".//div[@class='c16H9d']/a/text()")
+            title.append(title_temp[0])
+            price_current = product.xpath(".//span[@class='c13VH6']/text()")
+            format_price = (int)(price_current[0][:-2].replace(".",""))
+            price.append(format_price)
+            url_product_one = 'https://' + (product.xpath(".//div[@class='c16H9d']/a/@href")[0])[6:]
+            url_product.append(url_product_one)
+            category.append(category_url)
+    data = {'Category':category,'Name':title,'Price':price,'Url':url_product}
+    temp = pandas.DataFrame(data)
+    temp.to_csv('out_put.txt')            
     
 
     
